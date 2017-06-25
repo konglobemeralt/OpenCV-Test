c2095be63e8beb7088e235fbb0fa60f2c53a929c
Konglobemeralt <jesper.blidkvist@live.se>
2017-06-25 16:45:43 +0200

    Added some test files and finished four points transform

--- python/testTransform.py
+++ python/testTransform.py
@@ -1,5 +1,5 @@
 # import the necessary packages
-from pyimagesearch.transform import four_point_transform
+from transform import four_point_transform
 import numpy as np
 import argparse
 import cv2
