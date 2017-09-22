6ada4fd09a326c233b3528b675bba8c411caafac
Konglobemeralt <jesper.blidkvist@live.se>
2017-09-22 13:55:45 +0200

    Motion detection

--- MOPEDCV/MopedTests1.py
+++ MOPEDCV/MopedTests1.py
@@ -1,24 +1,40 @@
 ﻿import cv2
 
+#Diff images at times t0, t1, t2 to find change
+def diffImg(t0, t1, t2):
+    d1 = cv2.absdiff(t2, t1)
+    d2 = cv2.absdiff(t1, t0)
+    return cv2.bitwise_and(d1, d2)
+
+
 cam = cv2.VideoCapture(0)
 s, img = cam.read()
 
 windowName = "Vroom, Vroom"
 cv2.namedWindow('Front Camera')
 
+#Read three images, convert to greyscale (limit color space)
+tMinus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+tZero = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+tPlus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+
+
+
+
 print ("Tänk på hur du kör!")
 
 while s:
-    cv2.imshow(windowName, img)
-    s, img = cam.read()
+    cv2.imshow(windowName, diffImg(tMinus, tZero, tPlus))
+
+    # Read next image
+    tMinus = tZero
+    tZero = tPlus
+    tPlus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+
 
     key = cv2.waitKey(10)
     if key == 27: ##ESCAPE 
         cv2.destroyWindow(windowName)
         break
 
-
-
-
-
-
+print ("Sen var de bara nio!")
