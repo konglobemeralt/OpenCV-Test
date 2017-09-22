dc0dd7af8b8abef1e49a412276072753c81a5a30
Konglobemeralt <jesper.blidkvist@live.se>
2017-09-22 13:46:11 +0200

    Fixed some small issues

--- MOPEDCV/MopedTests1.py
+++ MOPEDCV/MopedTests1.py
@@ -1,4 +1,4 @@
-import cv2
+﻿import cv2
 
 cam = cv2.VideoCapture(0)
 s, img = cam.read()
@@ -6,13 +6,15 @@ s, img = cam.read()
 windowName = "Vroom, Vroom"
 cv2.namedWindow('Front Camera')
 
+print ("Tänk på hur du kör!")
+
 while s:
     cv2.imshow(windowName, img)
-
     s, img = cam.read()
 
+    key = cv2.waitKey(10)
     if key == 27: ##ESCAPE 
-        cv2.destroyWindow(winName)
+        cv2.destroyWindow(windowName)
         break
 
-print ("Goodbye")
+
